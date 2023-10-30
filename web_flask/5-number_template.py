8c39dfbb7ecdaaf36f146e400aafc04db4e917b3
BcodezMUJJE <billbrightmateg@gmail.com>
2023-10-30 02:45:42 -0700

    n5-number_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""thic starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'.
@@ -9,42 +9,54 @@ Routes:
     /number/<n>: Displays 'n is a number' only if <n> is an integer.
     /number_template/<n>: Displays an HTML page only if <n> is an integer.
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c_route(text):
+def c(text):
+    """Displays 'C' followed by the value of <text>
+    Replaces any underscores in <text> with slashes.
+    """
     text = text.replace("_", " ")
     return "C {}".format(text)
 
 
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python_route(text="is cool"):
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>
+    Replaces any underscores in <text> with slashes.
+    """
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
-def number_route(n):
+def number(n):
+    """Displays 'n is a number' only if <n> is an integer."""
     return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    return render_template("5-number_template.html", n=n)
+    """Displays an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
