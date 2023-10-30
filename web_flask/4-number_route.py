9de5cfdb22fad3d28d1cf1feae0ba1c71bd05b27
BcodezMUJJE <billbrightmateg@gmail.com>
2023-10-30 02:42:43 -0700

    number_routes

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""this starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'.
@@ -12,27 +12,33 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+
 @app.route("/python/<text>", strict_slashes=False)
 def python_route(text="is cool"):
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_route(n):
     return "{} is a number".format(n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
